cddc1ac394d6b9326e5735e0672df34eb8844072
Alexandra Andronescu <alexandra.andronescu@avira.com>
2014-03-25 17:02:14 +0200

    Refactor currency code

--- curs.py
+++ curs.py
@@ -2,30 +2,44 @@ import sys
 import datetime
 import requests
 
-def main():
-	# compute the medium value of each day's currency between the specified dates
-	date1 = date2 = None
-	today = datetime.datetime.now().date()
-	if len(sys.argv)==1:
+def check arguments(argv):
+	if len(argv)==1:
 		date1 = datetime.date(year=datetime.datetime.now().date().year, month=datetime.datetime.now().date().month, day=1)
 		date2 = today
 
 
 	try:
-		if len(sys.argv)>1:
-			if sys.argv[1]=="today":
+		if len(argv)>1:
+			if argv[1]=="today":
 				print "Today's currency: %s" % get_currency(today)
 				return
-			date1 = datetime.datetime.strptime(sys.argv[1], "%Y-%m-%d").date()
-			if len(sys.argv)>2:
-				date2 = datetime.datetime.strptime(sys.argv[2], "%Y-%m-%d").date()
+			date1 = datetime.datetime.strptime(argv[1], "%Y-%m-%d").date()
+			if len(argv)>2:
+				date2 = datetime.datetime.strptime(argv[2], "%Y-%m-%d").date()
 			else:
 				date2 = today
 		if not date1 or not date2:
-			raise ValueError
-	except ValueError:
-		print "Incorrect dates!"
-		print "Usage: python curs.py 2013-12-01 2013-12-20"
+			print "Incorrect dates!"
+			print "Usage: python curs.py 2013-12-01 2013-12-20"
+			return None, None
+	except Exception:
+		print "Exception while processing the parameters %s" % argv
+		return None, None
+
+	return date1, date2
+
+def get_currency(date):
+	return float(requests.get('http://www.infovalutar.ro/bnr/%d/%d/%d/EUR' % (date.year, date.month, date.day)).text)
+
+
+def get_currency():
+	# compute the medium value of each day's currency between the specified dates
+	date1 = date2 = None
+	today = datetime.datetime.now().date()
+
+	date1, date2 = check arguments(sys.argv)
+
+	if not date1 or not date2:
 		return
 
 	# order the dates
@@ -50,12 +64,11 @@ def main():
 	print "Computing the currency between %s and %s (%s working days/%s total days)" % (date1, date2, len(currency), difference.days)
 
 	median_currency = sum(currency)/len(currency)
-	if len(currency) < 40:
+	if len(currency) < 50:
 		print currency
+
 	print "Median currency: %s" % median_currency
 
-def get_currency(date):
-	return float(requests.get('http://www.infovalutar.ro/bnr/%d/%d/%d/EUR' % (date.year, date.month, date.day)).text)
 
 if __name__ == "__main__":
-	main()
+	get_currency()
